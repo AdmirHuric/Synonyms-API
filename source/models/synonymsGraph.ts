6c02bc1d6470fc04306697417c73a5580ce1280c
Admir Huric <huricadmir@hotmail.com>
2023-09-18 13:09:48 +0200

    Removed commented code

--- source/models/synonymsGraph.ts
+++ source/models/synonymsGraph.ts
@@ -18,7 +18,6 @@ export default class SynonymsGraph {
     deleteSynonym(word: string, synonym: string) {
         if (this.graph.has(word) && this.graph.has(synonym)) {
             this.removeEdge(word, synonym);
-            //this.removeEdge(synonym, word);
         }
     }
 
